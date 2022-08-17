99e32e537e8cee1fb16d16dc6fc33ee4d5b4d37f
Gabriel <gabriel.scardozo7@gmail.com>
2022-08-17 14:24:34 -0300

    criada-funcao-perguntarquantidadecartas

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function cliquei(){
-    prompt('Hello World!');
+
+
+
+
+let quantidadecartas = prompt("Digite a quantidade de cartas desejadas (mín 4; máx 14):");
+
+perguntarquantidade();
+
+function perguntarquantidade(){
+    while(quantidadecartas < 4 || quantidadecartas%2 !== 0 || quantidadecartas > 14){
+        quantidadecartas = prompt("Digite a quantidade de cartas desejadas (mín 4; máx 14):");
+    }
 }
+
+
